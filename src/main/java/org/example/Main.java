77936153f2a46ae7c271328ec16083f2ab4d68ac
sg36sagargupta26 <sg36.sagargupta26@gmail.com>
2024-12-02 22:42:16 +0530

    2nd commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.example.advent.of.code.day1.Day1;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Day1 day1 = new Day1();
+    }
+}
