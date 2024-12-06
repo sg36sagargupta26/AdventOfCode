146c0e18a8f88b862cf4ac81c357e4b148f10b00
sg36sagargupta26 <sg36.sagargupta26@gmail.com>
2024-12-06 15:38:00 +0530

    Day 4a backup

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,10 @@
 package org.example;
 
-import org.example.advent.of.code.day3.Day3b;
+import org.example.advent.of.code.day4.Day4a;
 
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new Day3b().sumOfMul());
+        System.out.println(new Day4a().totalCount());
     }
 }
