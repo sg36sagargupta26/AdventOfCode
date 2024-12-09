67f6dfb47175c72cf0aff2f31793fe1cf58d383a
sg36sagargupta26 <sg36.sagargupta26@gmail.com>
2024-12-09 15:11:53 +0530

    Day 6b

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,10 @@
 package org.example;
 
-import org.example.advent.of.code.day6.Day6b;
+import org.example.advent.of.code.day7.Day7a;
 
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new Day6b().totalCount());
+        System.out.println(new Day7a().safeReports());
     }
 }
---
+++ src/main/java/org/example/advent/of/code/day7/Day7a.java
@@ -0,0 +1,57 @@
+package org.example.advent.of.code.day7;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Stream;
+
+public class Day7a {
+    Path filePath = Paths.get("src/main/java/org/example/advent/of/code/day7/day7resource.txt");
+
+    public long safeReports(){
+        AtomicLong totalSafeReports = new AtomicLong();
+        try (Stream<String> lines = Files.lines(filePath)) {
+            lines.forEach(line -> {
+                List<Long> list = Arrays.stream(line.split(" |: "))
+                        .mapToLong(Long::parseLong)
+                        .boxed()
+                        .toList();
+
+                if(isValid(list,list.getFirst(),1,list.size()-1)){
+                    totalSafeReports.getAndAdd(list.getFirst());
+                }
+            });
+        } catch (IOException e) {
+            System.out.println("failed to read file");
+        }
+        return totalSafeReports.get();
+    }
+
+    private boolean isValid(List<Long> l, long target, long start, long end) {
+        if(start==end && target==l.getLast()){
+            return true;
+        }
+
+        if (target < 0) {
+            return false;
+        }
+        if (start + 1 > end) {
+            return false;
+        }
+        //addition
+        List<Long> list = new ArrayList<>(l);
+        long nextElement = list.get((int) (start + 1));
+        long add = list.get((int) start) + nextElement;
+        list.set((int) (start + 1), add);
+        boolean isValidAdd = isValid(list, target , start + 1, end);
+        long mul = list.get((int) start) * nextElement;
+        list.set((int) (start + 1), mul);
+        boolean isValidMul = isValid(list, target , start + 1, end);
+        return isValidAdd || isValidMul;
+    }
+}
